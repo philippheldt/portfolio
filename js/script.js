bcef5c7902fe56577b6e06f6de1178a6b17441e6
Philipp Jenny <register@philippjenny.de>
2022-02-02 17:07:56 +0100

    Setting up front page

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// lottie.destroy();
+lottie.loadAnimation({
+  container: document.getElementById("bm"),
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path: "animation/data1.json",
+});
+
+setTimeout(() => {
+  lottie.destroy();
+  lottie.loadAnimation({
+    container: document.getElementById("bm"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "animation/data2.json",
+  });
+}, 1000);
+
+// setTimeout(() => {
+//   //hier könnte man die Animation wieder rückwärts spielen lassen, wenn man runter scrollt mit inview
+//   lottie.destroy();
+//   lottie.loadAnimation({
+//     container: document.getElementById("bm"),
+//     renderer: "svg",
+//     loop: false,
+//     autoplay: true,
+//     path: "animation/data3.json",
+//   });
+// }, 10000);
