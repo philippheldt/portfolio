87f70f1bda1cfd3f86492b64c2e3c932315b6056
Philipp Jenny <register@philippjenny.de>
2022-02-04 12:02:48 +0100

    refactoring logo animation

--- animation_test/script.js
+++ animation_test/script.js
@@ -3,8 +3,11 @@ var animation1 = lottie.loadAnimation({
   renderer: "svg",
   loop: false,
   autoplay: false,
-  path: "../animation/data1_black.json",
+  path: "../animation/logo_white.json",
 });
-animation1.play();
-setTimeout
-animation1.goToAndStop(100, false);
+
+window.addEventListener("load", function () {
+  animation1.playSegments([0, 23], true);
+  animation1.playSegments([23, 139], true);
+  animation1.playSegments([139, 159], true);
+});
--- js/logo_anim.js
+++ js/logo_anim.js
@@ -1,49 +1,40 @@
-inView("#bm").on("enter", () => {
-  //hier könnte man die Animation wieder rückwärts spielen lassen, wenn man runter scrollt mit inview
-  lottie.destroy();
-  lottie.loadAnimation({
+window.addEventListener("load", function () {
+  var animation1 = lottie.loadAnimation({
     container: document.getElementById("bm"),
     renderer: "svg",
     loop: false,
-    autoplay: true,
-    path: "animation/data1.json",
+    autoplay: false,
+    path: "animation/logo_white.json",
   });
 
-  setTimeout(() => {
-    lottie.destroy();
-    lottie.loadAnimation({
-      container: document.getElementById("bm"),
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      path: "animation/data2.json",
+  var animation2 = lottie.loadAnimation({
+    container: document.getElementById("bm-2"),
+    renderer: "svg",
+    loop: false,
+    autoplay: false,
+    path: "animation/logo_black.json",
+  });
+
+  function loopMiddleWhite() {
+    animation1.playSegments([23, 139], false);
+  }
+  var loopWhite;
+  const desktopHeader = document.querySelector(".navigation");
+
+  inView.threshold(1);
+  inView.offset(0);
+
+  inView("#bm")
+    .on("enter", () => {
+      animation2.playSegments([147, 159], true);
+      animation1.playSegments([0, 23], true);
+      loopWhite = setInterval(loopMiddleWhite, 3000);
+      desktopHeader.classList.remove("visible");
+    })
+    .on("exit", () => {
+      clearInterval(loopWhite);
+      animation1.playSegments([147, 159], true);
+      desktopHeader.classList.add("visible");
+      animation2.playSegments([0, 23], true);
     });
-  }, 1000);
-
-  lottie.loadAnimation({
-    container: document.getElementById("bm-2"),
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-    path: "animation/data2_black.json",
-  });
-});
-
-inView("#profile").on("enter", () => {
-  //hier könnte man die Animation wieder rückwärts spielen lassen, wenn man runter scrollt mit inview
-  lottie.destroy();
-  lottie.loadAnimation({
-    container: document.getElementById("bm"),
-    renderer: "svg",
-    loop: false,
-    autoplay: true,
-    path: "animation/data3.json",
-  });
-  lottie.loadAnimation({
-    container: document.getElementById("bm-2"),
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-    path: "animation/data2_black.json",
-  });
 });
