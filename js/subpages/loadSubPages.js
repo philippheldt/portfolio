1747af890b80066da9f8d8d8ea99b59da4f4158a
Philipp Jenny <register@philippjenny.de>
2022-04-06 16:39:53 +0200

    adding Button Color Property and inserting Ajacent HTML instead of innerHTML

--- js/subpages/loadSubPages.js
+++ js/subpages/loadSubPages.js
@@ -8,35 +8,32 @@ fetch("js/subpages/starbucks.json")
   .then((res) => res.json())
   .then((data) => (obj = data))
   .then(() => {
-    console.log(obj.articles[0].titleText);
-    console.log(document.querySelector("#img1-0"));
     obj.articles.forEach((article) => {
-      switch (obj.articles[i].presetName) {
+      switch (article.presetName) {
         case "reg_3_large":
-          app.innerHTML = `<section id="portfolio" class="g-10">
+          app.insertAdjacentHTML(
+            "beforeend",
+            `<section id="portfolio" class="g-10">
             <div class="gallery g-6 c1-6">
-              <img src="" class="c-r1-4" id="img1-${i}" alt="" />
-              <img src="" class="c5-6" id="img2-${i}" alt="" />
-              <img src="" class="c5-6" id="img3-${i}" alt="" />
+              <img src="${article.img_1}" class="c-r1-4" id="img1-${i}" alt="" />
+              <img src="${article.img_2}" class="c5-6" id="img2-${i}" alt="" />
+              <img src="${article.img_3}" class="c5-6" id="img3-${i}" alt="" />
             </div>
             <div class="text c7-10">
-              <h2 id="title-${i}"></h2>
-              <p id="text-${i}"></p>
-              <button class="b-black" id="button-${i}"></button>
+              <h2 id="title-${i}">${article.title ? article.titleText : ""}</h2>
+              <p id="text-${i}">${article.regText}</p>
+              <button class="${article.buttonColor}" id="button-${i}">
+              ${article.button ? article.buttonText : ""}</button>
             </div>
-          </section>`;
-          document.querySelector("#img1-" + i).setAttribute("src", obj.articles[0].img_1);
-          document.querySelector("#img2-" + i).setAttribute("src", obj.articles[0].img_2);
-          document.querySelector("#img3-" + i).setAttribute("src", obj.articles[0].img_3);
-          if (obj.articles[i].title) {
-            document.querySelector("#title-" + i).innerHTML = obj.articles[0].titleText;
-          }
-          document.querySelector("#text-" + i).innerHTML = obj.articles[0].regText;
-          obj.articles[i].button
-            ? (document.querySelector("#button-" + i).innerHTML = obj.articles[0].buttonText)
-            : (document.querySelector("#button-" + i).style.display = "none");
+          </section>`
+          );
+          !article.title
+            ? (document.querySelector("#title-" + i).style.display = "none")
+            : (document.querySelector("#title-" + i).style.display = "grid");
+          !article.button
+            ? (document.querySelector("#button-" + i).style.display = "none")
+            : (document.querySelector("#button-" + i).style.display = "grid");
       }
-      //Achtung! Hier muss das innerHTML element appended werden, damit das alte nich überschrieben wird!
       i++;
     });
   });
